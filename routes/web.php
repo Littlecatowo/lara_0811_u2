5304c1e65c09423c0f4506b0bab51182483ed83b
Littlecatowo <tobytw3@gmail.com>
2025-10-29 05:16:23 +0800

    adding some route that r1 to r2

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "welcome by Test";
+});
+
+Route::get("/", function () {
+    return view("welcome");
+});
+
+Route::get("r1", function () {
+    return redirect("r2");
+});
+
+Route::get("r2", function () {
+    return view("welcome");
 });
