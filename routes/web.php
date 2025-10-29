34b9f41841592e44fb1780d3f3d137d346979d58
M514-T\admin <@>
2025-10-29 11:18:35 +0800

    adding HomeControll that Routing to home.index

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -31,3 +32,5 @@ Route::group(['prefix'=> 'admin'], function () {
         return 'admin dashboard';
     });
 });
+
+Route::get('home', [HomeController::class,'index'])->name('home.index');
