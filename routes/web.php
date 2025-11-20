b55ab6a334cfd6c842a601737c2e3f6f2c65d090
Littlecatowo <tobytw3@gmail.com>
2025-11-21 07:51:40 +0800

    adding home Route that Controller by HomeController

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,7 @@ Route::group(["prefix" => "admin"], function() {
         return 'admin dashboard';
     });
 });
+
+use App\Http\Controllers\HomeController;
+
+Route::get('home', [HomeController::class, 'index'])->name('home.index');
