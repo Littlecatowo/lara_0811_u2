97c33e9f140c5a60efd010ba8ade42407304818a
Littlecatowo <tobytw3@gmail.com>
2025-11-21 07:32:10 +0800

    adding Route r1 redirect to r2

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ Route::get("/", function() {
 Route::get("/", function() {
     return "welcome";
 });
+
+Route::get("r1", function() {
+    return redirect(("r2"));
+});
+
+Route::get("r2", function() {
+    return view("welcome");
+});
