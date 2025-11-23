9dd26891c5aeeb3d7573472b2dbc8feb0d064bc6
Littlecatowo <tobytw3@gmail.com>
2025-11-24 01:44:21 +0800

    打個註解

--- routes/web.php
+++ routes/web.php
@@ -14,32 +14,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get("/", function() {
-    return view("welcome");
+    return view("welcome"); #retun welcome page
 });
 
 Route::get("/", function() {
-    return "welcome";
+    return "welcome"; #return welcome string
 });
 
 Route::get("r1", function() {
-    return redirect(("r2"));
+    return redirect(("r2")); #redirect to r2 route
 });
 
 Route::get("r2", function() {
-    return view("welcome");
+    return view("welcome"); #return welcome page
 });
 
 Route::get("hello/{name?}", function($name= "Everyone") {
-    return "Hello, " . $name;
-}) -> name("hello.index");
+    return "Hello, " . $name; #return "Hello, XXX" string
+}) -> name("hello.index"); #named route to hello.index
 
 Route::get("dashboard", function() {
-    return 'dashboard';
+    return 'dashboard'; #return dashboard string 
 });
 
 Route::group(["prefix" => "admin"], function() {
     Route::get("dashboard", function() {
-        return 'admin dashboard';
+        return 'admin dashboard'; #return admin dashboard string
     });
 });
 
